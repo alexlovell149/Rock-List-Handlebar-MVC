aa323c4c076ca332cb2093f052cbc1bf6822d683
Alex Lovell <alexlovell@MacBook-Pro.local>
2017-05-21 16:16:17 -0400

    first push

---
+++ db/schema.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE bandsdb;
+
+USE bandsdb;
+
+CREATE TABLE bands (
+id INT NOT NULL AUTO_INCREMENT,
+band_name VARCHAR(250) NOT NULL,
+hall_of_fame BOOLEAN default false,
+date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+PRIMARY KEY (id)
+);
---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO bands (band_name, hall_of_fame) VALUES ('Pink Floyd', true);
+INSERT INTO bands (band_name, hall_of_fame) VALUES ('Nirvana', true);
+INSERT INTO bands (band_name, hall_of_fame) VALUES ('The Doors', true);
+INSERT INTO bands (band_name, hall_of_fame) VALUES ('Talking Heads', true);
+INSERT INTO bands (band_name, hall_of_fame) VALUES ('Fleetwood Mac', true);
